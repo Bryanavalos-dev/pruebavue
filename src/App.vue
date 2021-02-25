6769f61e209903afe2242ae8e3e526b00560c303
Bryanavalos-dev <73485085+Bryanavalos-dev@users.noreply.github.com>
2021-02-24 23:41:28 -0600

    agregue la tarjetita tal

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class=" flex flex-col text-xs bg-red-600">
-    <span>Hola</span>
-    <span>Hola</span>
-    <span>Hola</span>
-    <span>kike y erick se la comen</span>
+  <div class="h-screen w-screen flex flex-row items-center justify-center">
+    <div
+      class="border-2 border-red-900 flex flex-col h-96 w-56 justify-center items-center p-2 space-y-4"
+    >
+      <span>KIKE Y ERICK SE AMAN CON LOCURA</span>
+      <span>KIKE Y ERICK SE AMAN CON LOCURA</span>
+    </div>
   </div>
 </template>
